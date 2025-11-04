2801bfd45c321225ccf67d70e4eee9625943701d
bdripey-commits <bdripey@gmail.com>
2025-11-03 21:49:11 -0600

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,7 +1,3 @@
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/GameList.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
 
-local URL = Games[game.PlaceId]
 
-if URL then
-  loadstring(game:HttpGet(URL))()
-end
